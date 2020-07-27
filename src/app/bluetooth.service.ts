e8dd1093eaf7fffd3cfa3f695b9f8f27e4a4db32
Jitendra Singh <jitendra@hamrodev.com>
2020-07-27 18:15:44 +0545

    added new name to functions

--- src/app/bluetooth.service.ts
+++ src/app/bluetooth.service.ts
@@ -6,11 +6,11 @@ import { Injectable } from '@angular/core';
 export class BluetoothService {
 
   constructor() { }
-  getStatus():any{
+  getBatteryStatus():any{
     //connect to bluetooth and get battery status
     //return battery status
   }
-  sendKeyPress(data:any):void{
+  sendDataToDevice(data:any):void{
     //connect to bluetooth and sends the data of keyboard
   }
 }
