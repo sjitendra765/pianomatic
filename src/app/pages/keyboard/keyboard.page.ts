62451e52d79238f0840f67416df8b3d11940dfcf
Jitendra Singh <jitendra@hamrodev.com>
2020-08-04 13:27:46 +0545

    made appear dialogue box using component

--- src/app/pages/keyboard/keyboard.page.ts
+++ src/app/pages/keyboard/keyboard.page.ts
@@ -37,19 +37,30 @@ export class KeyboardPage implements OnInit {
     console.log(this.keyboardData[0].color)
   }
     // set to landscape
-    onKeyPress(event){
+    async onKeyPress(event){ //on pianoKey press
+     /* try{
+      if (this.renderer.selectRootElement('.dialogue')){
+        const prevDialogue = this.renderer.selectRootElement('.dialogue')
+        const destroPrevious = this.renderer.removeChild(this.renderer.parentNode(prevDialogue),prevDialogue)
+      }}
+      catch(err){
+        console.log(err)
+      } 
       const div = this.renderer.createElement('li');
-      const text = this.renderer.createText('Hello world!');
+      const text = this.renderer.createText("Hello World") //ToDo: Add the layout as required
       const parent = this.renderer.parentNode(event.target)
-      const child = this.renderer.nextSibling(event.target)
+      this.renderer.setStyle(parent, 'width','3700px') //increase the width when dialogue box appear
+      let child = this.renderer.nextSibling(event.target)
       this.renderer.appendChild(div, text);
       const cuEl = this.renderer.insertBefore(parent,div,child);
       const newEl = this.renderer.nextSibling(event.target )
       this.renderer.addClass(newEl, 'dialogue');
-    /*
-     this.service.setRootViewContainerRef(this.viewContainerRef)
+      
+      */
+    //this.renderer.setProperty(outsideBox, 'innerHTML', '<ng-template #dialogue class="dialogueBox"></ng-template>'); 
+    this.service.setRootViewContainerRef(this.viewContainerRef)
     this.service.addDynamicComponent()
-    */
+    
     }
 
 }
