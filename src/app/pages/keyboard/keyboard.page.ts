6748625d1f1c48bedb304ab76ec753ce67022176
Jitendra Singh <jitendra@hamrodev.com>
2020-08-07 16:24:27 +0545

    updated name of variable and scroll the screen

--- src/app/pages/keyboard/keyboard.page.ts
+++ src/app/pages/keyboard/keyboard.page.ts
@@ -56,13 +56,13 @@ export class KeyboardPage implements OnInit {
       const parentSpan = this.renderer.parentNode(event.target)
       const ul = this.renderer.parentNode(parentSpan)
       this.renderer.setStyle(ul, 'width','3630px') //increase the width when dialogue box appear
-      const next = this.renderer.nextSibling(event.target)
+      const dialogue = this.renderer.nextSibling(event.target)
       this.renderer.setAttribute(event.target,'class',this.keyboardData[idx].color+'Active') //remain keypress when key is clicked
       this.service.setRootViewContainerRef(this.widgetTargets.toArray()[idx])
       this.service.addDynamicComponent()
       // creating animation to open the dialogue when key pressed
       const animation = createAnimation()
-        .addElement(next)
+        .addElement(dialogue)
         .easing("ease-in-out")
         .duration(1000)
         .direction("alternate")
@@ -72,6 +72,9 @@ export class KeyboardPage implements OnInit {
         }).fromTo('transform','scale(0.25)','scale(1)')
         //.iterations(Infinity)
       animation.play();
+      console.log(event)
+      console.log(ul)
+      ul.scrollTo(500, 0);
       this.prevIdx = idx;
       this.prevKey = event.target;    
     
