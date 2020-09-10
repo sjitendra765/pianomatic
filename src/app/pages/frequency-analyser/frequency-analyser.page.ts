f47fe42d1c0e6cd7c3c6a1febe13337012f27c37
Jitendra Singh <jitendra@hamrodev.com>
2020-09-10 17:15:50 +0545

    latest

--- src/app/pages/frequency-analyser/frequency-analyser.page.ts
+++ src/app/pages/frequency-analyser/frequency-analyser.page.ts
@@ -7,39 +7,39 @@ import {FrequencyAnalyzerService} from '../../providers/frequency-analyzer.servi
   styleUrls: ['./frequency-analyser.page.scss'],
 })
 export class FrequencyAnalyserPage implements OnInit {
-  constructor(private freq: FrequencyAnalyzerService) {
+  constructor(private frequencyAnalyser: FrequencyAnalyzerService) {
    }
 
   ngOnInit() {
   }
 
   record(){
+    var that = this;
     const audioContext = new window.AudioContext();
     const analyser = audioContext.createAnalyser();
-    analyser.fftSize = 2048
+    // fftsize/sampleate = peroid of
+    analyser.fftSize = 4096;
     navigator.mediaDevices.getUserMedia(
                           {audio: true})
                           .then(stream => audioContext.createMediaStreamSource(stream).connect(analyser))
-                          .catch(err => console.log(err))
-
-
+                          .catch(err => console.log(err));
     const dataArray = new Float32Array(analyser.frequencyBinCount);
+    const period =  (analyser.fftSize / audioContext.sampleRate) * 1000;
     
-    var frequencyAnalyser = this.freq
- // this gets called via requestAnimationFrame, so runs roughly every 1s
+    // this gets called via requestAnimationFrame, so runs roughly every time the data buffer is refreshed
     setInterval(function () {
       analyser.getFloatTimeDomainData(dataArray);
-      var ac = frequencyAnalyser.autoCorrelate(dataArray, audioContext.sampleRate)
+      var ac = that.frequencyAnalyser.autoCorrelate(dataArray, audioContext.sampleRate)
       console.log(ac)
       if (ac == -1) {
       } else {
         var pitch = ac
-        var note =  frequencyAnalyser.noteFromPitch( pitch );
+        var note =  that.frequencyAnalyser.noteFromPitch( pitch );
         console.log("note", note)
-       var detune = frequencyAnalyser.centsOffFromPitch( pitch, note );
+       var detune = that.frequencyAnalyser.centsOffFromPitch( pitch, note );
        console.log("detune", detune)
      }
-  }, 50);
+  }, period);
 
   }
 
